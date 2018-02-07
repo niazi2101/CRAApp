ebfe659f3cda710015483ff6b293fa219a489550
hknia <hamza.khan.niazi04@gmail.com>
2018-02-07 10:05:50 +0500

    Initial commit

---
+++ app/src/main/java/com/iiui/craapp/activity/CrimeDetailActivity.java
@@ -0,0 +1,15 @@
+package com.iiui.craapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iiui.craapp.R;
+
+public class CrimeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_detail);
+    }
+}
